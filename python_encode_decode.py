874003365b19c77f8e252038fee093e51c19bfed
boramel022 <boramel022@student.wethinkcode.co.za>
2023-02-10 19:57:56 +0200

    Edit #2

--- python_encode_decode.py
+++ python_encode_decode.py
@@ -6,17 +6,17 @@ def encode(key, message):
     encoded = []
 
     """The remainder of division between i and len(key) uses the remainder 
-    as index of key, value of key at index is stored in key_c.
+    as index of key, value of key at index is stored in key_chars.
 
     The function ord() is used to get integer unicode value of unicode character (message), 
     then convert the value at index i into integer value. The remainder of division
-    of addition of ord(message[i]) and ord(key_c) with 256 is converted then stored in encoded.
+    of addition of ord(message[i]) and ord(key_chars) with 256 is converted then stored in encoded.
     
     encode() returns a utf-8 encoded message, decode() will decode the string"""
 
     for i in range(len(message)):
-        key_c = key[i % len(key)]
-        encoded.append(chr((ord(message[i]) + ord(key_c)) % 256))
+        key_charshars = key[i % len(key)]
+        encoded.append(chr((ord(message[i]) + ord(key_charshars)) % 256))
 
     return base64.urlsafe_b64encode("".join(encoded).encode()).decode()
 
@@ -32,8 +32,8 @@ def decode(key, message):
     value to string and store to decoded"""
 
     for i in range(len(message)):
-        key_c = key[i % len(key)]
-        decoded.append(chr((256 + ord(message[i]) - ord(key_c)) % 256))
+        key_chars = key[i % len(key)]
+        decoded.append(chr((256 + ord(message[i]) - ord(key_chars)) % 256))
 
     return "".join(decoded)
 
@@ -47,6 +47,8 @@ def mode(private_key, text, choice):
     elif choice.lower() == 'd' or choice.lower() == 'decode':
         result = decode(private_key, text)
         print(f'\nDecoded message: {result}')
+    else:
+        print('\nInvalid Mode!\n')
 
 
 def main():
@@ -56,6 +58,7 @@ def main():
     choice = input('Mode (Encode or Decode): ')
 
     mode(private_key, text, choice)
+
 
 if __name__ == '__main__':
     main()
